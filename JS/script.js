60414eb1f94a527feb91aaa04401d351d921c3dc
Dev Mishra <114310750+devm954@users.noreply.github.com>
2023-03-15 00:13:47 +0545

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+const togglebtn = document.querySelector('.toggle_btn')
+const togglebtnicon = document.querySelector('.toggle_btn i')
+const dropdownmenu = document.querySelector('.dropdown')
+
+togglebtn.onclick = function () {
+    dropdownmenu.classList.toggle('open')
+    const isopen = dropdownmenu.classList.contains('open')
+
+    togglebtnicon.classList = isopen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars '
+}
+
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let mountains_front = document.getElementById('mountains_front');
+let text = document.getElementById('intro');
+let btn = document.getElementById('btn');
+
+window.addEventListener('scroll',function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px';
+    mountains_front.style.left = value * 0 + 'px';
+    text.style.marginRight = value * 4 + 'px';
+    text.style.marginTop = value * 1.2 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+})
